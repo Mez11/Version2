68edeb4df82907a9e844c7f3ce545b790bfd74ed
Misael Alanis <misael.alanis@spir-it.net>
2016-10-17 11:35:42 -0500

    Se elimina la implementacion de codigo con Sockets (Todavia no se utilizan)

--- src/com/aplicaciones/practicas/uno/server/EnvioCatalogo.java
+++ src/com/aplicaciones/practicas/uno/server/EnvioCatalogo.java
@@ -4,114 +4,39 @@
  * and open the template in the editor.
  */
 package com.aplicaciones.practicas.uno.server;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.List;
 
 import com.redes.dao.ProductosDao;
 import com.redes.model.Productos;
-import com.redes.p2.servidor.view.AgregarProducto;
 import com.redes.p2.servidor.view.CatalogoDeProductos;
-import com.redes.p2.servidor.view.EditarProducto;
 /**
  *
  * @author 
  */
 public class EnvioCatalogo {
-    private ServerSocket serverSocket;
+	//En esta clase no se interactua con ningun cliente
+    //private ServerSocket serverSocket;
 	
-	/**
-	 *  Cierra la conexion con el socket }. 
-	**/
-	public void close( ){
-		if( serverSocket == null ){
-			System.out.println( "El servidor no se a inicializado" );
-		}
-		try {
-			serverSocket.close( );
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-        
-    public void catalogo(){
-		Socket socket = null;
-		DataInputStream dis = null;
-		DataOutputStream dos = null;
-        ObjectOutputStream oos = null;
+    //EnvioCatalogo se convierte en clase principal...
+    public static void main( String [] args ){
 		ProductosDao miProducto = new ProductosDao();
         List<Productos> list = null;
         CatalogoDeProductos catalogoDeProductos = new CatalogoDeProductos();
-        AgregarProducto agregarProducto = new AgregarProducto();
-        EditarProducto editarProducto = new EditarProducto();	
-                
-                
+        
+        System.out.println("Conectando a la base de datos");
+        miProducto.inicializarConexion();
+        
+        System.out.println("Obteniendo catalogo");
+        list = miProducto.getProductos();
+        
+        //CARGA CATALOGO DE PRODUCTOS
+        //EDIT: Al inicio, solo se debe mostrar el catalogo de productos
+        //(No los otros frames)
+        System.out.println( "Cargando vista de catalogos..." );
+        catalogoDeProductos.init( );
+        //agregarProducto.AgregarProducto();
+        //editarProducto.getImagen();
 		
-		if( serverSocket == null ){
-			System.out.println( "El servidor no a sido inicializado" );
-			return ;
-		}
-		
-		try {
-                    
-			System.out.println( "Esperando conexion @.%..." );
-                   
-            System.out.println("Conectando a la base de datos");
-            miProducto.inicializarConexion();
-            
-            System.out.println("Obteniendo catalogo");
-            list = miProducto.getProductos();
-            System.out.println("Agregando productos");
-            System.out.println("Editando productos");
-            //CARGA CATALOGO DE PRODUCTOS
-            //EDIT: Al inicio, solo se debe mostrar el catalogo de productos
-            //(No los otros frames)
-            catalogoDeProductos.init( );
-            //agregarProducto.AgregarProducto();
-            //editarProducto.getImagen();
-                      
-			socket = serverSocket.accept( );
-                        
-                       
-                       
-                       
-                        
-			System.out.println( "Conexion aceptada " + socket.getInetAddress( ).getHostAddress( ) );
-			oos= new ObjectOutputStream(socket.getOutputStream());
-                        dis = new DataInputStream( socket.getInputStream( ) );
-			dos = new DataOutputStream( socket.getOutputStream( ) );
-			
-                          System.out.println(list);
-                     
-			//Request the number of files being transfered...
-			oos.writeObject(list);
-                        System.out.println("Catalogo enviado");
-                        
-			dis.close( );
-			//socket.close( );
-			
-		} catch (IOException e) {
-			e.printStackTrace( );
-			return;
-		}
-		
-	}
-        
-        
-        
-        
-        public boolean init( int port ){
-		try {
-			serverSocket = new ServerSocket( port );
-                      
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
+        System.out.println(list);
 	}
 }
--- src/com/aplicaciones/practicas/uno/server/RunnerServer.java
+++ src/com/aplicaciones/practicas/uno/server/RunnerServer.java
@@ -1,29 +1,27 @@
 package com.aplicaciones.practicas.uno.server;
 
-//import com.aplicaciones.practicas.uno.MyFile;
-
 public class RunnerServer {
-	private static final int DEFAULT_PORT = 4070;
-	private static EnvioCatalogo envio;
+	//AVISO: No se use esta clase (todavia)
+	//Para ejecutar la aplicacion, corra "Enviocatalogo.java"
+	//private static final int DEFAULT_PORT = 4070;
 	
-	private static void addShutdownHook( ){
+	/*private static void addShutdownHook( ){
 		Runtime.getRuntime().addShutdownHook( new Thread( ){
 			public void run( ){
-				envio.close( );
+				//envio.close( );
 			}
 		});
-	}
+	}*/
 	
-	public static void main( String args[] ){
+	/*public static void main( String args[] ){
 		int port;
 		port = DEFAULT_PORT;
 		System.out.println( "Usando el puerto " + port );
-		envio = new EnvioCatalogo( );
 		if( !envio.init( port ) ){
 			System.out.println( "Previos errores, el envio no puede iniciar @.#.." );
 		}
-		envio.catalogo();
+		EnvioCatalogo.main( null );
 		addShutdownHook( );
-	}
+	}*/
 	
 }
